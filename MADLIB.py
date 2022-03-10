b61caa994e8a99dd63927a1e913cd9f3553c3644
Manasasuresh388 <101304512+Manasasuresh388@users.noreply.github.com>
2022-03-09 21:31:02 -0500

    Add files via upload

---
+++ MADLIB.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#******MADLIB GAME FOR BEGINEERS *****************
+#youtuber = "Manasa" #some string variable
+
+#print("subscribe to " + youtuber) # concatenate
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("Famous Person: ")
+    
+madlib = f"Computer programming is so {adj}! It makes so excited all the \
+time because I love to {verb1} .Stay hydrated and {verb2} like you are \
+    {famous_person}!"
+    
+print (madlib)
